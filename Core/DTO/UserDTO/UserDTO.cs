cf769787345c060bbe10c22615be12e72b4e2cec
tonia4ik-s <43933313+tonia4ik-s@users.noreply.github.com>
2023-06-18 17:55:45 +0300

    status in dto

--- Core/DTO/UserDTO/UserDTO.cs
+++ Core/DTO/UserDTO/UserDTO.cs
@@ -5,6 +5,6 @@ public class UserDTO
     public string Id { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-    public int StatusId { get; set; }
+    public int Status { get; set; }
     public int Points { get; set; }
 }
